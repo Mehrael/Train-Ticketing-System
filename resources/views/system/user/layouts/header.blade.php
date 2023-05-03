2c1e5b8241c72a43d560db591c07d3c095f081ab
Mehrael <mehraelashraf20@gmail.com>
2023-05-03 22:14:24 +0300

    logout from user logos

--- resources/views/system/user/layouts/header.blade.php
+++ resources/views/system/user/layouts/header.blade.php
@@ -39,9 +39,6 @@
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
         <a href="" class="logo d-flex align-items-center">
-            <!-- Uncomment the line below if you also wish to use an image logo -->
-            <!-- <img src="assets2/img/logo.png" alt=""> -->
-{{--            <h1>Logis</h1>--}}
         </a>
 
         <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
@@ -51,26 +48,7 @@
                 <li><a href="\" class="active">Home</a></li>
                 <li><a href="ViewTrainSchedules">Trains' Schedule</a></li>
                 <li><a href="Booking">Booking</a></li>
-                <li><a href="">Pricing</a></li>
-{{--                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>--}}
-{{--                    <ul>--}}
-{{--                        <li><a href="#">Drop Down 1</a></li>--}}
-{{--                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>--}}
-{{--                            <ul>--}}
-{{--                                <li><a href="#">Deep Drop Down 1</a></li>--}}
-{{--                                <li><a href="#">Deep Drop Down 2</a></li>--}}
-{{--                                <li><a href="#">Deep Drop Down 3</a></li>--}}
-{{--                                <li><a href="#">Deep Drop Down 4</a></li>--}}
-{{--                                <li><a href="#">Deep Drop Down 5</a></li>--}}
-{{--                            </ul>--}}
-{{--                        </li>--}}
-{{--                        <li><a href="#">Drop Down 2</a></li>--}}
-{{--                        <li><a href="#">Drop Down 3</a></li>--}}
-{{--                        <li><a href="#">Drop Down 4</a></li>--}}
-{{--                    </ul>--}}
-{{--                </li>--}}
-                <li><a href="">Contact</a></li>
-                <li><a class="get-a-quote" href="">Get a Quote</a></li>
+                <li><a class="get-a-quote" href="logout">Logout</a></li>
             </ul>
         </nav><!-- .navbar -->
 
